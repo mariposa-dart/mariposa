471caf3d60cdaf447c98f4736cdf5fa99f7592af
Tobe O <thosakwe@gmail.com>
2019-09-30 14:37:34 -0400

    Use universal in hydrate app

--- packages/angel_mariposa/example/hydrate_app/web/main.dart
+++ packages/angel_mariposa/example/hydrate_app/web/main.dart
@@ -1,5 +1,6 @@
 import 'package:hydrate_app/hydrate_app.dart';
-import 'package:mariposa/browser.dart';
+// import 'package:mariposa/browser.dart';
+import 'package:mariposa/universal.dart';
 import 'package:universal_html/html.dart';
 
 main() => runMariposaApp(() => HydrateApp(), querySelector('#app'));
